7dffcf33e5fea6be0e2642e84bbcd134777fafa8
Yoni Doron-Peters <yonidoronpeters@gmail.com>
2020-02-10 15:07:03 -0700

    Add authentication and authorization to restaurant API

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({
     // disables details message in response when validation error occurs
     disableErrorMessages: true
